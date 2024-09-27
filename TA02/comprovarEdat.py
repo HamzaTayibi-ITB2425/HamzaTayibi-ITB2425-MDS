a9849140a5781a0b78a018c272dfcce2064300f3
HamzaTayibi-ITB2425 <hamza.tayibi.7e6@itb.cat>
2024-09-27 12:39:46 +0200

    prova

---
+++ TA02/comprovarEdat.py
@@ -0,0 +1,14 @@
+"""
+Hamza Tayibi Soudani
+27/09/2024
+ASIX1A
+Descripció: Exemple dels apunts
+
+"""
+
+# Programa que demana l'edat i diu si ets major d'edat.
+edat=int(input("Quina edat tens?"))
+if edat>=18:
+    print("Ets major d'edat")
+
+print("Programa Finalitzat")
